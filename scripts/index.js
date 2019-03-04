4ddb947d28dc3accead9941b6f234832d23e8a63
Eddie Bueno <eddiebueno93@gmail.com>
2019-03-04 09:44:28 -0800

    added addItem to go through the thinkful api

--- scripts/index.js
+++ scripts/index.js
@@ -4,16 +4,12 @@
 $(document).ready(function() {
   shoppingList.bindEventListeners();
   shoppingList.render();
+  api.getItems()
+    .then(res => res.json())
+    .then((items) => {
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
+    });
 });
 
-store.items.push(Item.create('apples'));
-
-api.createItem('pears')
-  .then(res => res.json())
-  .then((newItem) => {
-    return api.getItems();
-  })
-  .then(res => res.json())
-  .then((items) => {
-    console.log(items);
-  });
+// store.items.push(Item.create('apples'));
